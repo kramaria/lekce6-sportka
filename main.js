793bc0a343fc769777d96aaf13c55a0be725d60b
maria-code341 <talanovama@gmail.com>
2021-04-20 16:29:33 +0200

    [FUNCTIONALITY] 2 delay between called numbers, button losuj, history of called numbers

--- main.js
+++ main.js
@@ -7,33 +7,47 @@
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
 
+
 let osudi = []
 // console.log(osudi);
 
-for (let i=1; i<=49; i=i+1 /*i+=1 i++ ++i */) {
-  osudi.push(i);
-}
-// console.log(osudi);
-
-// z osudi vyberu 7 nahodnych cisiel
-
-let tazenaCisla = [];
-
-for (let i = 0; i < 7; i++) {
-  let vyherniIndex = Math.floor(Math.random() * osudi.length);
-  let vyherniCislo = osudi[vyherniIndex];
-
-  tazenaCisla.push(vyherniCislo);
-  osudi.splice(vyherniIndex,1);
-
-  console.log(osudi);
-  console.log(tazenaCisla);
-}
-
-// vypisu do html
-
+let tlacitko = document.querySelector('.tlacitko')
 let vyherniCisla = document.querySelector('#vyherni-cisla');
 
-for (let i = 0; i < tazenaCisla.length; i++) {
-  vyherniCisla.innerHTML += '<span class ="cislo">' + tazenaCisla[i] + '</span>';
+function losuj() {
+  vyherniCisla.innerHTML = []
+  for (let i=1; i<=49; i=i+1 /*i+=1 i++ ++i */) {
+    osudi.push(i);
+  }
+  // console.log(osudi);
+
+  // z osudi vyberu 7 nahodnych cisiel
+
+  let tazenaCisla = [];
+
+  for (let i = 0; i < 7; i++) {
+    let vyherniIndex = Math.floor(Math.random() * osudi.length);
+    let vyherniCislo = osudi[vyherniIndex];
+
+    tazenaCisla.push(vyherniCislo);
+    osudi.splice(vyherniIndex,1);
+
+    console.log(osudi);
+    console.log(tazenaCisla);
+  }
+
+  // vypisu do html a nastavim casovac 2s pri losovani cisiel
+
+  
+
+  for (let i = 0; i < tazenaCisla.length; i++) {
+    setTimeout(function () {
+    vyherniCisla.innerHTML += '<span class ="cislo">' + tazenaCisla[i] + '</span>';
+    
+  }, i*2000)
+  // zapisem vylosovane cisla do zoznamu
+  
+  }
+  let historieHer = document.querySelector('#seznam'); 
+  historieHer.innerHTML += tazenaCisla + '<br>';
 }
